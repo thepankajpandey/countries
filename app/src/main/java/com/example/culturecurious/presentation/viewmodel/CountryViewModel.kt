1fd1d7b536a4ca38dda1388a61f30a58402f75ca
thepankajpandey <59520561+thepankajpandey@users.noreply.github.com>
2026-04-15 16:47:21 +0530

    Added test class of viewmodel

--- app/src/main/java/com/example/culturecurious/presentation/viewmodel/CountryViewModel.kt
+++ app/src/main/java/com/example/culturecurious/presentation/viewmodel/CountryViewModel.kt
@@ -7,6 +7,7 @@ import com.example.culturecurious.presentation.uistate.UiState
 import com.example.culturecurious.util.NetworkResult
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -16,7 +17,7 @@ class CountryViewModel @Inject constructor(
 ) : ViewModel() {
 
     private val _uiState = MutableStateFlow(UiState(isLoading = true))
-    val uiState = _uiState
+    val uiState: StateFlow<UiState> = _uiState
 
     init {
         fetchMovies()
--- app/src/test/java/com/example/culturecurious/presentation/viewmodel/CountryViewModelTest.kt
+++ app/src/test/java/com/example/culturecurious/presentation/viewmodel/CountryViewModelTest.kt
@@ -1,6 +1,82 @@
 package com.example.culturecurious.presentation.viewmodel
 
+import com.example.culturecurious.data.remote.dto.CountryDto
+import com.example.culturecurious.domain.repository.CountriesRepository
+import com.example.culturecurious.util.NetworkResult
+import io.mockk.clearAllMocks
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Before
+import org.junit.Test
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class CountryViewModelTest {
+    private lateinit var viewModel: CountryViewModel
+    private val repository: CountriesRepository = mockk()
 
+    private val testDispatcher = StandardTestDispatcher()
+
+    @Before
+    fun setup() {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+        clearAllMocks()
+    }
+
+    @Test
+    fun `when repository emits loading then uiState should be loading`() = runTest {
+        val flow = flow { emit(NetworkResult.Loading) }
+
+        coEvery { repository.getCountries() } returns flow
+
+        viewModel = CountryViewModel(repository)
+        testDispatcher.scheduler.advanceUntilIdle()
+
+        Assert.assertTrue(viewModel.uiState.value.isLoading)
+    }
+
+    @Test
+    fun `when repository emits success then uiState should have data`() = runTest {
+        // Given
+        /*val countries = listOf(
+            CountryDto(null,"India"),
+            Country("USA")
+        )*/
+    }
+
+    @Test
+    fun `when repository emits error then uiState should have error message`() = runTest {
+        // Given
+        val errorMessage = "Something went wrong"
+
+        val flow = flow {
+            emit(NetworkResult.Error(errorMessage))
+        }
+
+        coEvery { repository.getCountries() } returns flow
+
+        // When
+        viewModel = CountryViewModel(repository)
+        testDispatcher.scheduler.advanceUntilIdle()
+
+        // Then
+        val state = viewModel.uiState.value
+        assertFalse(state.isLoading)
+        assertEquals(errorMessage, state.errorMessage)
+    }
 }
